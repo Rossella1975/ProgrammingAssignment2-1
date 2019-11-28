7fcde9fa013707440cf0ffced041c4970daa99b9
Rossella1975 <58292416+Rossella1975@users.noreply.github.com>
2019-11-28 11:40:03 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+inversa <- NULL
 
+# set the matrix
+set <- function( matrix ) {
+rossella <<- matrix
+inversa <<- NULL
+}
+
+# get the matrix
+get <- function() {
+rossella
+}
+
+setInverse <- function(inverse) {
+inversa <<- inverse
+}
+
+getInverse <- function() {
+inversa
+}
+
+list(set = set, get = get,
+setInverse = setInverse,
+getInverse = getInverse)
 }
 
 
@@ -12,4 +35,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        rossella <- x$getInverse()
+
+if( !is.null(rossella) ) {
+
+message("getting cached inverse")
+
+return(rossella)
+
+}
+
+
+
+data <- x$get() ## Get x
+
+elisa <- solve(data,...) ##Solve 
+
+x$setInverse(elisa) ## Set the inverse 
+
+elisa ## Return the matrix
+
 }
